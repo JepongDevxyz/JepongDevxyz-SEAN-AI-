285d7beeda86d3a156bc16233643fd241d4356be
JepongDevxyz <businessdevgithubjepong@gmail.com>
2026-08-27 02:38:42 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -69,7 +69,7 @@ export default async function handler(req) {
       if (!key) return new Response('[LLAMA/GROQ ERROR]: Walang GROQ_API_KEYS.', { status: 200 });
       fetchUrl = 'https://api.groq.com/openai/v1/chat/completions';
       fetchHeaders['Authorization'] = `Bearer ${key}`;
-      fetchBody = { model: 'llama-4-scout-17b-16e-instruct', messages: cleanMessages };
+      fetchBody = { model: 'llama-3.3-70b-versatile', messages: cleanMessages };
     } 
 
     // 4. DEEPSEEK
@@ -96,7 +96,7 @@ export default async function handler(req) {
       if (!key) return new Response('[HUGGINGFACE ERROR]: Walang HUGGINGFACE_API_KEYS.', { status: 200 });
       fetchUrl = 'https://router.huggingface.co/hf-inference/v1/chat/completions';
       fetchHeaders['Authorization'] = `Bearer ${key}`;
-      fetchBody = { model: 'meta-llama/Llama-3.3-70B-Instruct', messages: cleanMessages };
+      fetchBody = { model: 'Qwen/Qwen2.5-72B-Instruct', messages: cleanMessages };
     } 
 
     // 7. COHERE
